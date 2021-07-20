e5d3e4e1fc036247d6a722b200b2b23ff4ee840a
chivoi <a.lastovirya@gmail.com>
2021-07-20 20:48:23 +1000

    change axios base uri to deployed staging version for testing

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const bookingSystemAPI = axios.create({
-  baseURL: 'http://localhost:3000'
+  // baseURL: 'http://localhost:3000'
+  baseURL: 'https://alex-velasco-book-staging.herokuapp.com'
 })
 
 bookingSystemAPI.interceptors.request.use(req => {
